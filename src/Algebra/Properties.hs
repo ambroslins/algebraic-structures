24f5ae7ca9e0d1228932720cce47690ff99be46d
ambroslins <ambros.lins@gmail.com>
2020-10-08 18:54:12 +0200

    add properties

---
+++ src/Algebra/Properties.hs
@@ -0,0 +1,47 @@
+module Algebra.Properties where
+
+import Algebra
+import Algebra.Prelude
+import Control.DeepSeq (NFData, deepseq)
+
+closed :: (NFData a, Magma a) => a -> a -> Bool
+closed x y = deepseq (x <> y) True
+
+associative :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+associative x y z = (x <> y) <> z == x <> (y <> z)
+
+leftDivision :: (Eq m, Quasigroup m) => m -> m -> Bool
+leftDivision x y = y == x <> (x <\> y) && y == x <\> (x <> y)
+
+rightDivision :: (Eq m, Quasigroup m) => m -> m -> Bool
+rightDivision x y = y == (y </> x) <> x && y == (y <> x) </> x
+
+division :: (Eq m, Quasigroup m) => m -> m -> Bool
+division x y = leftDivision x y && rightDivision x y
+
+leftIdentity :: (Eq a, IdentityElement a) => a -> Bool
+leftIdentity x = identityElement <> x == x
+
+rightIdentity :: (Eq a, IdentityElement a) => a -> Bool
+rightIdentity x = x <> identityElement == x
+
+identity :: (Eq a, IdentityElement a) => a -> Bool
+identity x = leftIdentity x && rightIdentity x
+
+commutative :: (Eq m, CommutativeSemigroup m) => m -> m -> Bool
+commutative x y = x <> y == y <> x
+
+leftInverse :: (Eq a, Group a) => a -> Bool
+leftInverse x = inverse x <> x == identityElement
+
+rightInverse :: (Eq a, Group a) => a -> Bool
+rightInverse x = x <> inverse x == identityElement
+
+leftDistributive :: (Eq m, CommutativeSemigroup (Product m), CommutativeSemigroup (Sum m)) => m -> m -> m -> Bool
+leftDistributive x y z = x * (y + z) == (x * y) + (x * z)
+
+rightDistributive :: (Eq m, CommutativeSemigroup (Product m), CommutativeSemigroup (Sum m)) => m -> m -> m -> Bool
+rightDistributive x y z = (y + z) * x == (y * x) + (z * x)
+
+distributive :: (Eq m, CommutativeSemigroup (Product m), CommutativeSemigroup (Sum m)) => m -> m -> m -> Bool
+distributive x y z = leftDistributive x y z && rightDistributive x y z
